df9798cfc9ea440f50a3f808e7f5a8d5fba62ae2
linhnguyen <nvlinh17041992@gmail.com>
2022-05-16 10:04:17 +0700

    fix

--- Project-API/magnews2-master/js/categories.js
+++ Project-API/magnews2-master/js/categories.js
@@ -1,120 +1,89 @@
 $(document).ready(function () {
-  const API_URL = "http://apiforlearning.zendvn.com/api/";
-  const API_CATEGORY = API_URL + "categories_news";
+  const API_URL = 'http://apiforlearning.zendvn.com/api/';
+  const API_CATEGORY = API_URL + 'categories_news';
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
-  const stringId = urlParams.getAll("id").toString();
-  const breadcrumb = $("#breadCrumb");
-  const pageheading = $("#pageHeading");
-  const title = $("#title");
-  const btnPre = $(".btn-pre");
-  const btnNext = $(".btn-next");
-  let categories = $("#zvn-menu-desktop");
-  let categoriesMobile = $("#zvn-menu-mobile");
-  let articles = $("#articles");
-  let feature1stArticle = $("#firstArticle");
+  const stringId = urlParams.getAll('id').toString();
+  const breadcrumb = $('#breadCrumb');
+  const pageheading = $('#pageHeading');
+  const title = $('#title');
+  const btnPre = $('.btn-pre');
+  const btnNext = $('.btn-next');
+  let categories = $('#zvn-menu-desktop');
+  let categoriesMobile = $('#zvn-menu-mobile');
+  let articles = $('#articles');
+  let feature1stArticle = $('#firstArticle');
   let subFeature = $('#sub-articles');
   let offset = 5;
   let itemOfPage = 10;
   let goldContent = $('#get-gold');
-  let coinContent = $('#coin-content')
+  let coinContent = $('#coin-content');
+
+  let arr = [
+    
+  ];
+
+  console.log(arr.findIndex(fruit => fruit.article_id === 2));
 
   getItems();
 
-  function getItems() {
-    $.ajax({
-      type: "GET",
-      url: API_CATEGORY + `/${stringId}` + "/articles",
-      data: { offset: offset, limit: itemOfPage },
-      dataType: "json",
-      beforeSend: function () {
-        articles.html('<img width="500" src="images/0_ptDX0HfJCYpo9Pcs.gif">');
-      },
-      success: function (response) {
-        let content = "";
-        response.forEach(function (ele) {
-          content += `
-          <div class="col-sm-6 p-r-25 p-r-15-sr991">
-            <div class="m-b-45">
-                <a href="detail.html?id=${ele.id}" class="wrap-pic-w hov1 trans-03">
-                  <img src="${ele.thumb}">
-                </a>
-                <div class="p-t-16">
-                  <h5 class="p-b-5">
-                    <a href="detail.html?id=${ele.id}" class="f1-m-3 cl2 hov-cl10 trans-03">
-                      ${ele.title}
-                    </a>
-                  </h5>
-                    <span class="f1-s-3">
-                      ${ele.publish_date}
-                    </span>
-                  </div>
-            </div>
-          </div>
-          `;
-        });
-        articles.html(content);
-      },
+  
+
+    btnNext.on('click', function () {
+        btnPre.removeClass('btn-disable');
+        offset = offset + 10;
+        getItems();
     });
-  }
 
-  btnNext.on("click", function () {
-    offset = offset + 10;
-    getItems();
-  });
+    btnPre.on('click', function () {
+        offset -= 10;
+        if (offset === 5) $(this).addClass('btn-disable');
+        getItems();
+    });
 
-  btnPre.on("click", function () {
-    if (offset){
-      $(this).addClass('btn-disable')
-    }
-    getItems();
-  });
+    $('#btn-search').on('click', function () {
+        let value = $('#search').val();
+        window.location.href = 'searchresults.html?keyword=' + value;
+    });
 
-  $("#btn-search").on("click", function () {
-    let value = $("#search").val();
-    window.location.href = "searchresults.html?keyword=" + value;
-  });
-
-  $.ajax({
-    type: "GET",
-    url: API_CATEGORY + `/${stringId}`,
-    dataType: "json",
-    success: function (response) {
-      let content = "";
-      let article = "";
-      let subArticles = "";
-      content += `
-      <a href="index.html" class="breadcrumb-item f1-s-3 cl9">
-					Trang chủ
-				</a>
-				<span class="breadcrumb-item f1-s-3 cl9">
-					${response.name}
-				</span>
-      `;
-      let pageHeading = "";
-      pageHeading += `
+    $.ajax({
+        type: 'GET',
+        url: API_CATEGORY + `/${stringId}`,
+        dataType: 'json',
+        success: function (response) {
+            let content = '';
+            let article = '';
+            let subArticles = '';
+            content += `
+              <a href="index.html" class="breadcrumb-item f1-s-3 cl9">
+                  Trang chủ
+                </a>
+                <span class="breadcrumb-item f1-s-3 cl9">
+                  ${response.name}
+                </span>
+              `;
+            let pageHeading = '';
+            pageHeading += `
       <h2 class="f1-l-1 cl2">
 			${response.name}
 		  </h2>
       `;
 
-      let webtitle = "";
-      webtitle += `
+            let webtitle = '';
+            webtitle += `
       <title>${response.name}</title>
       `;
-      $.ajax({
-        type: "GET",
-        url: API_CATEGORY + `/${stringId}` + "/articles",
-        data: { offset: 0, limit: 1 },
-        dataType: "json",
-        beforeSend: function () {
-          feature1stArticle.html(
-            '<img width="500" src="images/0_ptDX0HfJCYpo9Pcs.gif">'
-          );
-        },
-        success: function (feature) {
-          feature.forEach(function (ele) {
-            article += `
+            $.ajax({
+                type: 'GET',
+                url: API_CATEGORY + `/${stringId}` + '/articles',
+                data: { offset: 0, limit: 1 },
+                dataType: 'json',
+                beforeSend: function () {
+                    feature1stArticle.html('<img width="500" src="images/0_ptDX0HfJCYpo9Pcs.gif">');
+                },
+                success: function (feature) {
+                    feature.forEach(function (ele) {
+                        article += `
             <div class="bg-img1 size-a-3 how1 pos-relative"
 						style="background-image: url(${ele.thumb});">
 						<a href="detail.html?id=${ele.id}" class="dis-block how1-child1 trans-03"></a>
@@ -127,26 +96,23 @@ $(document).ready(function () {
 							</h3>
 						</div>
 					</div>
-            `
-            ;
-          });
-          feature1stArticle.html(article);
-        },
-      });
+            `;
+                    });
+                    feature1stArticle.html(article);
+                },
+            });
 
-      $.ajax({
-        type: "GET",
-        url: API_CATEGORY + `/${stringId}` + "/articles",
-        data: { offset: 1, limit: 4 },
-        dataType: "json",
-        beforeSend: function () {
-          subFeature.html(
-            '<img width="500" src="images/0_ptDX0HfJCYpo9Pcs.gif">'
-          );
-        },
-        success: function (subfeature) {
-          subfeature.forEach(function (ele) {
-            subArticles += `
+            $.ajax({
+                type: 'GET',
+                url: API_CATEGORY + `/${stringId}` + '/articles',
+                data: { offset: 1, limit: 4 },
+                dataType: 'json',
+                beforeSend: function () {
+                    subFeature.html('<img width="500" src="images/0_ptDX0HfJCYpo9Pcs.gif">');
+                },
+                success: function (subfeature) {
+                    subfeature.forEach(function (ele) {
+                        subArticles += `
             <div class="col-sm-6 p-rl-1 p-b-2">
 							<div class="bg-img1 size-a-14 how1 pos-relative"
 								style="background-image: url(${ele.thumb});">
@@ -162,61 +128,59 @@ $(document).ready(function () {
 								</div>
 							</div>
 						</div>
-            `
-            ;
-          });
-          subFeature.html(subArticles);
+            `;
+                    });
+                    subFeature.html(subArticles);
+                },
+            });
+
+            title.html(webtitle);
+            pageheading.html(pageHeading);
+            breadcrumb.html(content);
         },
-      });
+    });
 
+    $.ajax({
+        type: 'GET',
+        url: API_CATEGORY,
+        data: { offset: 0, limit: 20 },
+        dataType: 'json',
+        success: function (response) {
+            let content = '';
+            let oMenu = '';
+            response.forEach(function (ele, index) {
+                let menuItem = `<li><a href="category.html?id=${ele.id}" class="disable-after">${ele.name}</a></li>`;
+                if (index < 4) {
+                    content += menuItem;
+                } else {
+                    oMenu += menuItem;
+                }
+            });
+            if (oMenu) {
+                content += ` <li><a href="#">Danh mục khác</a> <ul class="sub-menu">${oMenu}</ul></li>`;
+            }
+            categories.html(content);
+        },
+    });
 
-      title.html(webtitle);
-      pageheading.html(pageHeading);
-      breadcrumb.html(content);
-    },
-  });
-
-  $.ajax({
-    type: "GET",
-    url: API_CATEGORY,
-    data: { offset: 0, limit: 20 },
-    dataType: "json",
-    success: function (response) {
-      let content = "";
-      let oMenu = "";
-      response.forEach(function (ele, index) {
-        let menuItem = `<li><a href="category.html?id=${ele.id}" class="disable-after">${ele.name}</a></li>`;
-        if (index < 4) {
-          content += menuItem;
-        } else {
-          oMenu += menuItem;
-        }
-      });
-      if (oMenu) {
-        content += ` <li><a href="#">Danh mục khác</a> <ul class="sub-menu">${oMenu}</ul></li>`;
-      }
-      categories.html(content);
-    },
-  });
-
-  $.ajax({
-    type: "GET",
-    url: API_CATEGORY,
-    data: { offset: 0, limit: 20 },
-    dataType: "json",
-    success: function (response) {
-      let content = "";
-      let oMenu = "";
-      response.forEach(function (ele, index) {
-        let menuItem = `<li><a href="category.html?id=${ele.id}" class="category">${ele.name}</a></li>`;
-        if (index < 4) {
-          content += menuItem;
-        } else {
-          oMenu += menuItem;
-        }
-      });
-      if (oMenu) {
-        content += `
+    $.ajax({
+        type: 'GET',
+        url: API_CATEGORY,
+        data: { offset: 0, limit: 20 },
+        dataType: 'json',
+        success: function (response) {
+            let content = '';
+            let oMenu = '';
+            response.forEach(function (ele, index) {
+                let menuItem = `<li><a href="category.html?id=${ele.id}" class="category">${ele.name}</a></li>`;
+                if (index < 4) {
+                    content += menuItem;
+                } else {
+                    oMenu += menuItem;
+                }
+            });
+            if (oMenu) {
+                content += `
               <li>
                   <a href="#">Danh mục khác</a>
                   <ul class="sub-menu-m">
@@ -227,19 +191,19 @@ $(document).ready(function () {
 						      </span>
               </li>
           `;
-      }
-      categoriesMobile.html(content);
-    },
-  });
+            }
+            categoriesMobile.html(content);
+        },
+    });
 
-  $.ajax({
-    type: "GET",
-    url: API_URL + "get-gold",
-    dataType: "json",
-    success: function (response) {
-      let content = '';
-      response.forEach(function(ele){
-        content += `
+    $.ajax({
+        type: 'GET',
+        url: API_URL + 'get-gold',
+        dataType: 'json',
+        success: function (response) {
+            let content = '';
+            response.forEach(function (ele) {
+                content += `
 							<ul class="p-t-15">
 								<li class="flex-wr-sb-c p-b-20">
 
@@ -260,23 +224,21 @@ $(document).ready(function () {
 									</div>  
 								</li>
 							</ul>
-        `
-      });
-      goldContent.html(content);
-    }
-  });
+        `;
+            });
+            goldContent.html(content);
+        },
+    });
 
-  $.ajax({
-    type: "GET",
-    url: API_URL + "get-coin",
-    dataType: "json",
-    success: function alo (response) {
-      let content = '';
-      response.forEach(function (ele){
-        function parse () {
-    
-        }
-        content += `
+    $.ajax({
+        type: 'GET',
+        url: API_URL + 'get-coin',
+        dataType: 'json',
+        success: function alo(response) {
+            let content = '';
+            response.forEach(function (ele) {
+                function parse() {}
+                content += `
         <li class="flex-wr-sb-c p-b-20">
 									<div class="size-w-3 flex-wr-sb-c">
 										<p href="#" class="f1-s-9 text-uppercase cl3">
@@ -296,14 +258,51 @@ $(document).ready(function () {
                     </p>
                   </div>
 				</li>
-        `
-      });
-      coinContent.html(content);
-    }
-  });
+        `;
+            });
+            coinContent.html(content);
+        },
+    });
 
-  $(document).on("click", ".arrow-main-menu-m", function () {
-    $(this).parent().find(".sub-menu-m").slideToggle();
-    $(this).toggleClass("turn-arrow-main-menu-m");
-  });
+    $(document).on('click', '.arrow-main-menu-m', function () {
+        $(this).parent().find('.sub-menu-m').slideToggle();
+        $(this).toggleClass('turn-arrow-main-menu-m');
+    });
+
+    function getItems() {
+      $.ajax({
+        type: 'GET',
+        url: API_CATEGORY + `/${stringId}` + '/articles',
+        data: { offset: offset, limit: itemOfPage },
+        dataType: 'json',
+        beforeSend: function () {
+          articles.html('<img width="500" src="images/0_ptDX0HfJCYpo9Pcs.gif">');
+        },
+        success: function (response) {
+          let content = '';
+          response.forEach(function (ele) {
+              content += `
+                <div class="col-sm-6 p-r-25 p-r-15-sr991">
+                  <div class="m-b-45">
+                      <a href="detail.html?id=${ele.id}" class="wrap-pic-w hov1 trans-03">
+                        <img src="${ele.thumb}">
+                      </a>
+                      <div class="p-t-16">
+                        <h5 class="p-b-5">
+                          <a href="detail.html?id=${ele.id}" class="f1-m-3 cl2 hov-cl10 trans-03">
+                            ${ele.title}
+                          </a>
+                        </h5>
+                          <span class="f1-s-3">
+                            ${ele.publish_date}
+                          </span>
+                        </div>
+                  </div>
+                </div>
+                `;
+          });
+          articles.html(content);
+        },
+      });
+    }
 });
--- Project-API/magnews2-master/js/index.js
+++ Project-API/magnews2-master/js/index.js
@@ -1,55 +1,59 @@
 $(document).ready(function () {
-  const API_URL = "http://apiforlearning.zendvn.com/api/";
-  const API_CATEGORY = API_URL + "categories_news";
-  const API_ARTICLES = API_URL + "articles";
-  let categories = $("#zvn-menu-desktop");
-  let categoriesMobile = $("#zvn-menu-mobile");
-  let newestBlog = $("#newest");
-  let lastestBlog = $("#lastest");
-  let goldContent = $("#get-gold");
-  let coinContent = $("#coin-content");
-  $.ajax({
-    type: "GET",
-    url: API_CATEGORY,
-    data: { offset: 0, limit: 20 },
-    dataType: "json",
-    success: function (response) {
-      let content = "";
-      let oMenu = "";
-      response.forEach(function (ele, index) {
-        let menuItem = `<li><a href="category.html?id=${ele.id}" class="disable-after">${ele.name}</a></li>`;
-        if (index < 4) {
-          content += menuItem;
-        } else {
-          oMenu += menuItem;
-        }
-      });
-      if (oMenu) {
-        content += ` <li><a href="#">Danh mục khác</a> <ul class="sub-menu">${oMenu}</ul></li>`;
-      }
-      content += `<li><a href="favourite.html" class="disable-after">Danh sách yêu thích</a></li>`;
-      categories.html(content);
-    },
-  });
+    const API_URL = 'http://apiforlearning.zendvn.com/api/';
+    const API_CATEGORY = API_URL + 'categories_news';
+    const API_ARTICLES = API_URL + 'articles';
+    let categories = $('#zvn-menu-desktop');
+    let categoriesMobile = $('#zvn-menu-mobile');
+    let newestBlog = $('#newest');
+    let lastestBlog = $('#lastest');
+    let goldContent = $('#get-gold');
+    let coinContent = $('#coin-content');
 
-  $.ajax({
-    type: "GET",
-    url: API_CATEGORY,
-    data: { offset: 0, limit: 20 },
-    dataType: "json",
-    success: function (response) {
-      let content = "";
-      let oMenu = "";
-      response.forEach(function (ele, index) {
-        let menuItem = `<li><a href="category.html?id=${ele.id}" class="category">${ele.name}</a></li>`;
-        if (index < 4) {
-          content += menuItem;
-        } else {
-          oMenu += menuItem;
-        }
-      });
-      if (oMenu) {
-        content += `
+    let favItem = JSON.parse(localStorage.getItem('FAV_LIST')) || [];
+    console.log(favItem);
+
+    $.ajax({
+        type: 'GET',
+        url: API_CATEGORY,
+        data: { offset: 0, limit: 20 },
+        dataType: 'json',
+        success: function (response) {
+            let content = '';
+            let oMenu = '';
+            response.forEach(function (ele, index) {
+                let menuItem = `<li><a href="category.html?id=${ele.id}" class="disable-after">${ele.name}</a></li>`;
+                if (index < 4) {
+                    content += menuItem;
+                } else {
+                    oMenu += menuItem;
+                }
+            });
+            if (oMenu) {
+                content += ` <li><a href="#">Danh mục khác</a> <ul class="sub-menu">${oMenu}</ul></li>`;
+            }
+            content += `<li><a href="favourite.html" class="disable-after">Danh sách yêu thích</a></li>`;
+            categories.html(content);
+        },
+    });
+
+    $.ajax({
+        type: 'GET',
+        url: API_CATEGORY,
+        data: { offset: 0, limit: 20 },
+        dataType: 'json',
+        success: function (response) {
+            let content = '';
+            let oMenu = '';
+            response.forEach(function (ele, index) {
+                let menuItem = `<li><a href="category.html?id=${ele.id}" class="category">${ele.name}</a></li>`;
+                if (index < 4) {
+                    content += menuItem;
+                } else {
+                    oMenu += menuItem;
+                }
+            });
+            if (oMenu) {
+                content += `
                     <li>
                         <a href="#">Danh mục khác</a>
                         <ul class="sub-menu-m">
@@ -60,31 +64,29 @@ $(document).ready(function () {
                         </span>
                     </li>
                 `;
-      }
-      categoriesMobile.html(content);
-    },
-  });
+            }
+            categoriesMobile.html(content);
+        },
+    });
 
-  $.ajax({
-    type: "GET",
-    url: API_ARTICLES,
-    data: { offset: 0, limit: 3 },
-    dataType: "json",
-    beforeSend: function () {
-      newestBlog.html(
-        '<img width="500" src="images/0_ptDX0HfJCYpo9Pcs.gif">'
-      );
-    },
-    success: function (response) {
-      let content = "";
-      response.forEach(function (ele) {
-        let idCategory = ele.category_id;
-        $.ajax({
-          type: "GET",
-          url: API_CATEGORY + `/${idCategory}`,
-          dataType: "json",
-          success: function (response) {
-            content += `
+    $.ajax({
+        type: 'GET',
+        url: API_ARTICLES,
+        data: { offset: 0, limit: 3 },
+        dataType: 'json',
+        beforeSend: function () {
+            newestBlog.html('<img width="500" src="images/0_ptDX0HfJCYpo9Pcs.gif">');
+        },
+        success: function (response) {
+            let content = '';
+            response.forEach(function (ele) {
+                let idCategory = ele.category_id;
+                $.ajax({
+                    type: 'GET',
+                    url: API_CATEGORY + `/${idCategory}`,
+                    dataType: 'json',
+                    success: function (response) {
+                        content += `
               <div class="col-md-4 p-rl-1 p-b-2">
                 <div class="bg-img1 size-a-11 how1 pos-relative" style="background-image: url(${ele.thumb});">
                   <a href="detail.html?id=${ele.id}" class="dis-block how1-child1 trans-03"></a>
@@ -101,76 +103,59 @@ $(document).ready(function () {
                 </div>
               </div>
               `;
-            newestBlog.html(content);
-          },
-        });
-      });
-    },
-  });
-
-  function loadLastestArticle() {
-    let favItem = JSON.parse(localStorage.getItem("FAV_LIST")) || [];
-    $.ajax({
-      type: "GET",
-      url: API_ARTICLES,
-      data: { offset: 0, limit: 6 },
-      dataType: "json",
-      success: function (response) {
-        let content = "";
-        response.forEach(function (ele, index) {
-          if (index > 2) {
-            let isActive =
-              favItem.indexOf(`${ele.id}`) != -1 ? "active-icon-fav" : "";
-            content += `
-              <div class="col-sm-6 col-md-4">
-                <div class="m-b-45">
-                    <a href="detail.html?id=${ele.id}" class="wrap-pic-w hov1 trans-03">
-                      <img src="${ele.thumb}" alt="IMG">
-                    </a>
-                    <a id="${ele.id}"><i class="fa-solid fa-heart icon-wishlist ${isActive}" id="heartWish${ele.id}"></i></a>
-                  <div class="p-t-16">
-                    <h5 class="p-b-5">
-                      <a href="detail.html?id=${ele.id}" class="f1-m-3 cl2 hov-cl10 trans-03">
-                        ${ele.title}
-                      </a>
-                    </h5>
-                    <span class="f1-s-3">
-                      ${ele.publish_date}
-                    </span>
-                  </div>
-                </div>
-              </div>
-              `;
-            lastestBlog.html(content);
-            $(document).on("click", `#${ele.id}`, function () {
-              if (favItem.indexOf(`${ele.id}`) != -1) {
-                $(this).find(`#heartWish${ele.id}`).removeClass("active-icon-fav");
-                favItem = jQuery.grep(favItem, function (value) {
-                  return value != `${ele.id}`;
+                        newestBlog.html(content);
+                    },
                 });
-                localStorage.setItem("FAV_LIST", JSON.stringify(favItem));
-              } else {
-                $(this).find(`#heartWish${ele.id}`).addClass("active-icon-fav");
-                favItem.push(`${ele.id}`);
-                localStorage.setItem("FAV_LIST", JSON.stringify(favItem));
-              }
             });
-          }
-        });
-      },
+        },
     });
-  }
 
-  $.ajax({
-    type: "GET",
-    url: API_URL + "get-gold",
-    dataType: "json",
-    success: function (response) {
-      let content = "";
-      response.forEach(function (ele) {
-        let price = ele.buy;
-        let priceRound = price.toFixed(2);
-        content += `
+    function loadLastestArticle() {
+        $.ajax({
+            type: 'GET',
+            url: API_ARTICLES,
+            data: { offset: 3, limit: 3 },
+            dataType: 'json',
+            success: function (response) {
+                let content = '';
+                response.forEach(function (ele, index) {
+                    let isActive = favItem.indexOf(ele.id) != -1 ? 'active-icon-fav' : '';
+                    content += `
+                    <div class="col-sm-6 col-md-4">
+                      <div class="m-b-45">
+                          <a href="detail.html?id=${ele.id}" class="wrap-pic-w hov1 trans-03">
+                            <img src="${ele.thumb}" alt="IMG">
+                          </a>
+                          <a data-id="${ele.id}" class="btn-like-article"><i class="fa-solid fa-heart icon-wishlist ${isActive}" id="heartWish${ele.id}"></i></a>
+                        <div class="p-t-16">
+                          <h5 class="p-b-5">
+                            <a href="detail.html?id=${ele.id}" class="f1-m-3 cl2 hov-cl10 trans-03">
+                              ${ele.title}
+                            </a>
+                          </h5>
+                          <span class="f1-s-3">
+                            ${ele.publish_date}
+                          </span>
+                        </div>
+                      </div>
+                    </div>
+                    `;
+                    lastestBlog.html(content);
+                });
+            },
+        });
+    }
+
+    $.ajax({
+        type: 'GET',
+        url: API_URL + 'get-gold',
+        dataType: 'json',
+        success: function (response) {
+            let content = '';
+            response.forEach(function (ele) {
+                let price = parseFloat(ele.buy);
+                let priceRound = price.toFixed(2);
+                content += `
 							<ul class="p-t-15">
 								<li class="flex-wr-sb-c p-b-20">
 
@@ -192,20 +177,20 @@ $(document).ready(function () {
 								</li>
 							</ul>
         `;
-      });
-      goldContent.html(content);
-    },
-  });
+            });
+            goldContent.html(content);
+        },
+    });
 
-  $.ajax({
-    type: "GET",
-    url: API_URL + "get-coin",
-    dataType: "json",
-    success: function alo(response) {
-      let content = "";
-      response.forEach(function (ele) {
-        function parse() {}
-        content += `
+    $.ajax({
+        type: 'GET',
+        url: API_URL + 'get-coin',
+        dataType: 'json',
+        success: function alo(response) {
+            let content = '';
+            response.forEach(function (ele) {
+                function parse() {}
+                content += `
         <li class="flex-wr-sb-c p-b-20">
 									<div class="size-w-3 flex-wr-sb-c">
 										<p href="#" class="f1-s-9 text-uppercase cl3">
@@ -226,15 +211,30 @@ $(document).ready(function () {
                   </div>
 				</li>
         `;
-      });
-      coinContent.html(content);
-    },
-  });
+            });
+            coinContent.html(content);
+        },
+    });
 
-  loadLastestArticle();
+    loadLastestArticle();
 
-  $(document).on("click", ".arrow-main-menu-m", function () {
-    $(this).parent().find(".sub-menu-m").slideToggle();
-    $(this).toggleClass("turn-arrow-main-menu-m");
-  });
+    $(document).on('click', '.arrow-main-menu-m', function () {
+        $(this).parent().find('.sub-menu-m').slideToggle();
+        $(this).toggleClass('turn-arrow-main-menu-m');
+    });
+
+    $(document).on('click', '.btn-like-article', function () {
+        let id = $(this).data('id');
+        console.log(id, favItem);
+        if (favItem.indexOf(id) != -1) {
+            $(this).find(`#heartWish${id}`).removeClass('active-icon-fav');
+            favItem = jQuery.grep(favItem, function (value) {
+                return value != id;
+            });
+        } else {
+            $(this).find(`#heartWish${id}`).addClass('active-icon-fav');
+            favItem.push(id);
+        }
+        localStorage.setItem('FAV_LIST', JSON.stringify(favItem));
+    });
 });
